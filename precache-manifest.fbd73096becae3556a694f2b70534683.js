19c032126ba1da572a5efe7aec9a471f6810e711
HENGYI YU <y11235813l@gmail.com>
2020-08-04 11:16:09 -0700

    Updates

---
+++ precache-manifest.fbd73096becae3556a694f2b70534683.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39313be98cfbb45122d6747bd2d05949",
+    "url": "/StarLink/index.html"
+  },
+  {
+    "revision": "e6abad2763fd0f2d3579",
+    "url": "/StarLink/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "cf0b79ba31585ad12ead",
+    "url": "/StarLink/static/css/main.28187b1a.chunk.css"
+  },
+  {
+    "revision": "e6abad2763fd0f2d3579",
+    "url": "/StarLink/static/js/2.6d444153.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/StarLink/static/js/2.6d444153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0b79ba31585ad12ead",
+    "url": "/StarLink/static/js/main.34baf73d.chunk.js"
+  },
+  {
+    "revision": "3b60808a37604d0ec746",
+    "url": "/StarLink/static/js/runtime-main.ee516df9.js"
+  },
+  {
+    "revision": "142c8c7ffbd4f05b42de3a347ada6a45",
+    "url": "/StarLink/static/media/Starlink_Logo.142c8c7f.svg"
+  },
+  {
+    "revision": "07c6d56168cce19b0c46c57deb607a8b",
+    "url": "/StarLink/static/media/satellite.07c6d561.svg"
+  }
+]);
